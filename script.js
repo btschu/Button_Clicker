ba7f58e45a2c823065b99fee10b10c8e2cf01111
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-27 19:33:08 -0700

    added extra functionality to the buttons

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+// login/logout button change back and forth when clicked
+
 function logOut(element) {
-    element.innerText = "Logout";
+    if (element.innerText == "Login") {
+        element.innerText = "Logout"
+    } else {
+        element.innerText = "Login";
+    }
 }
+
+// hide "add definition" button when clicked
 
 function hide(element) {
     element.remove();
+}
+
+// first like button increases by 1 and alerts that it was liked when clicked
+
+let addLike1 = document.querySelector("#addLike1")
+let likeCount1 = 13
+
+function ninjaAlert1() {
+    likeCount1++
+    addLike1.innerText = likeCount1
+    alert("Ninja was liked!")
+}
+
+// second like button increases by 1 and alerts that it was liked when clicked
+
+let addLike2 = document.querySelector("#addLike2")
+let likeCount2 = 37
+
+function ninjaAlert2() {
+    likeCount2++
+    addLike2.innerText = likeCount2
+    alert("Ninja was liked!")
 }
